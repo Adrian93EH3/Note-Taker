aae1a1d3f440eae92e4a7107105d939ac2f915ca
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-02 22:10:17 -0400

    adding some notes I had saved and will make a ScreenCastify soon to display it working and my attempt at a delete note function

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-// Dependencies
+// DEPENDENCIES
 // =============================================================
 const express = require("express");
 const path = require("path");
 const dbPath = require("./db/db.json");
+const fs = require("fs");
 
-// Sets up the Express App
+// SETS UP THE EXPRESS APP
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Sets up the Express app to handle data parsing
+// SETS UP THE EXPRESS DATA TO HANDLE THE DATA PARSING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Routes
+// ROUTES DOWN BELOW
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// BASIC ROUTE THATS SENDS THE USER FIRST TO THE AJAX PAGE
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -30,20 +32,38 @@ app.get("/api/notes", function (req, res) {
     res.json(dbPath);
 });
 
-app.post("api/notes", function (req, res) {
-    console.log(req, body)
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    note.id = dbPath.length + 1;
+    dbPath.push(note)
+
+    // FOLLOWING CODE SAVES THE NOTE TO THE FRONT-END
+    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dbPath), "utf8", function(err) {
+        if(err) throw err;
+        res.json(dbPath)
+    })
 });
 
+// COMING BACK FOR FUTURE DEVELOPMENT, THIS IS MY ATTEMPT AT THE DELETE
 app.delete("api/notes/:id", function (req, res) {
-    res.sendFile("Delete request at /user")
+    res.deleteFile("Delete request at /user");
+    const noteDelete = req.body;
+    noteDelete.id = dbPathlength -= 1;
+    dbPath.push(noteDelete)
+
+   
+    fs.deleteFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dbPath), "utf8", function(err) {
+        if(err) throw err;
+        res.json(dbPath)
+    })
 });
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, index.html))
 });
 
-// Starts the server to begin listening
+// STARTS THE SERVER TO BEGIN LISTENING
 // =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on http:/localhost" + PORT)
+    console.log("App listening on http://localhost:" + PORT)
 });
